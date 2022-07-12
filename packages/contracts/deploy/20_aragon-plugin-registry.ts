2a63406c94475ed94747fe209762225acc337615
Rekard0 <5880388+Rekard0@users.noreply.github.com>
2022-07-12 12:54:31 +0200

    update and add deploy script

--- packages/contracts/deploy/20_aragon-plugin-registry.ts
+++ packages/contracts/deploy/20_aragon-plugin-registry.ts
@@ -28,5 +28,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   });
 };
 export default func;
-func.runAtTheEnd = true;
 func.tags = ['AragonPluginRegistry'];
--- packages/contracts/deploy/30_repo-factory.ts
+++ packages/contracts/deploy/30_repo-factory.ts
@@ -33,5 +33,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   );
 };
 export default func;
-func.runAtTheEnd = true;
 func.tags = ['AragonPluginRegistry'];
